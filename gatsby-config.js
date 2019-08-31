d2e72beed10a47a7767f3eae22c696690e3a60f3
jslee <jslee@solidware.io>
2019-08-31 16:21:09 +0900

    Add typography.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,13 @@ module.exports = {
         name: `markdown-pages`,
         path: `${__dirname}/src/markdown-pages`
       },
-    }
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/components/layout.styles.ts
+++ src/components/layout.styles.ts
@@ -1,16 +1,8 @@
 import { css } from "@emotion/core"
 
 export const GlobalStyles = css`
-html {
-  font-family: sans-serif;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-}
-body {
 
-}
 html {
-  font: 112.5%/1.45em georgia, serif;
   box-sizing: border-box;
   overflow-y: scroll;
 }
@@ -25,16 +17,6 @@ html {
 }
 body {
   margin: 0;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: hsla(0, 0%, 0%, 0.8);
-  font-family: georgia, serif;
-  font-weight: normal;
   word-wrap: break-word;
-  font-kerning: normal;
-  -moz-font-feature-settings: "kern", "liga", "clig", "calt";
-  -ms-font-feature-settings: "kern", "liga", "clig", "calt";
-  -webkit-font-feature-settings: "kern", "liga", "clig", "calt";
-  font-feature-settings: "kern", "liga", "clig", "calt";
 }
 `
